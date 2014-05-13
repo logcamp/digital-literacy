28e00013540fca887f59418fba4d5518f2674cdf
robertraleigh <robert@logcamp.org>
2014-05-13 14:49:16 -0700

    Building the digital literacy content

---
+++ js/header.js
@@ -0,0 +1,10 @@
+function HeaderController ($scope) {
+  $scope.data = {
+    headingOne: 'Use an H2 heading here',
+    paragraphOne: 'Insert some of your own paragraph text here.',
+    paragraphTwo: 'Insert some of your own paragraph text here.',
+    headerImage: 'http://placehold.it/480x297',  
+    actionOne: 'Action Button One', 
+    actionTwo: 'Action Button Two'
+  }       
+}
